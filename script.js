5785a675f03a51ef345b1b112fc618911c562110
KrisStrat <krisstrat@yandex.ru>
2021-10-22 12:37:16 +0300

    java script added

---
+++ script.js
@@ -0,0 +1,17 @@
+let x = getElementById('buy');
+let c = getElementById('close');
+let i = getElementById('pop_up')
+function open() {
+ i = getElementById('pop_up') ;
+ i.style.display = "block";
+}
+function close() {
+  i.style.display = "none";
+}
+ x.onclick = open;
+
+if (i.style.display = "block") {
+  c.onclick = close() {
+    i.style.display = "none";
+  }
+}
