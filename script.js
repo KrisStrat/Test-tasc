d6dc8555760f20934e4eda197d3e1d92afa9c6af
KrisStrat <krisstrat@yandex.ru>
2021-10-22 12:55:09 +0300

    changed css and html

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let x = document.getElementById('buy');
 let c = document.getElementById('close');
-let i = document.getElementById('pop_up')
+let i = document.getElementById('pop_up');
 function open() {
  i.style.display = "block";
 }
 function close() {
   i.style.display = "none";
 }
- x.onclick = open;
+ x.onclick = open();
 
 if (i.style.display = "block") {
   c.onclick = close();
