e8d458846c455f0a0af3c67e3edb6cdc5e12c9d5
KrisStrat <krisstrat@yandex.ru>
2021-10-24 12:21:25 +0300

    the last change in the script

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-let x = document.getElementById('buy');
-let c = document.getElementById('close');
-let i = document.getElementById('pop_up');
-function open() {
- i.style.display = "block";
-}
-function close() {
-  i.style.display = "none";
-}
- x.addEventListener("click", open);
+window.onload = function(){
+	let x = document.getElementById('buy');
+	let c = document.getElementById('close');
+	let i = document.getElementById('pop_up');
 
-if (i.style.display === "block") {
-  c.addEventListener("click" , close);
+	function open() {
+	 i.style.display = "block";
+	}
+	function close() {
+	  i.style.display = "none";
+	}
+
+	x.addEventListener("click", function(event){
+	  open();
+	  event.preventDefault()
+
+	});
+
+	if (i.style.display = "block") {
+	  c.addEventListener("click",function(event){
+      close();
+      event.preventDefault()
+
+    });
+	}
+
 }
